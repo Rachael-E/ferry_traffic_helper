4a89ee3514efc2d30604091fd9ab85e4a1529920
Rachael Ellen <rellen@esri.com>
2024-10-15 22:35:23 +0100

    use value notifier boolean instead of boolean

--- lib/ferry_traffic_tracker.dart
+++ lib/ferry_traffic_tracker.dart
@@ -32,7 +32,10 @@ class _FerryTrafficScreenState extends State<FerryTrafficScreen> {
   late Route route;
   FerrySchedule ferrySchedule = FerrySchedule();
   Polyline? routeGeometry;
-  bool isRouteGeometryInitialized = false;
+  // bool isRouteGeometryInitialized = false;
+  ValueNotifier<bool> isRouteGeometryInitializedNotifier =
+      ValueNotifier<bool>(false);
+
   bool isTimeChosen = false;
   RossOfMullPointsData? selectedPlace;
   TimeOfDay selectedTime = TimeOfDay.fromDateTime(DateTime.now());
@@ -42,7 +45,6 @@ class _FerryTrafficScreenState extends State<FerryTrafficScreen> {
       "https://route-api.arcgis.com/arcgis/rest/services/World/Route/NAServer/Route_World",
     ),
   );
-
 
   @override
   Widget build(BuildContext context) {
@@ -101,16 +103,23 @@ class _FerryTrafficScreenState extends State<FerryTrafficScreen> {
                           return Theme(
                             data: ThemeData.light().copyWith(
                               colorScheme: ColorScheme.light(
-                                primary: Colors.teal, // Header background color (selected)
-                                onPrimary: Colors.white, // Header text color (selected time)
-                                onSurface: Colors.teal[900]!, // Text color for unselected time
+                                primary: Colors
+                                    .teal, // Header background color (selected)
+                                onPrimary: Colors
+                                    .white, // Header text color (selected time)
+                                onSurface: Colors.teal[
+                                    900]!, // Text color for unselected time
                               ),
                               timePickerTheme: TimePickerThemeData(
-                                backgroundColor:Colors.teal[50], // Time picker background
+                                backgroundColor:
+                                    Colors.teal[50], // Time picker background
                                 hourMinuteTextColor: WidgetStateColor
-                                    .resolveWith((states) => states.contains(WidgetState.selected)
-                                        ? Colors.white // Selected time text color
-                                        : Colors.teal[900]!), // Unselected time text color
+                                    .resolveWith((states) => states
+                                            .contains(WidgetState.selected)
+                                        ? Colors
+                                            .white // Selected time text color
+                                        : Colors.teal[
+                                            900]!), // Unselected time text color
                                 dialHandColor:
                                     Colors.teal[600], // Dial hand color
                                 dialBackgroundColor:
@@ -183,9 +192,7 @@ class _FerryTrafficScreenState extends State<FerryTrafficScreen> {
                     index % 2 == 0 ? Colors.teal[50]! : Colors.teal[100]!;
 
                 return GestureDetector(
-                  // title: Text(rossOfMullPointInfo.name),
                   onTap: () {
-                    // When a location is selected
                     changeViewpointToTappedPoint(rossOfMullPointInfo.point);
                     createRouteStops(rossOfMullPointInfo.point);
                     generateRoute(pointStops);
@@ -238,62 +245,70 @@ class _FerryTrafficScreenState extends State<FerryTrafficScreen> {
     showDialog(
       context: context,
       builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Text("Confirm your selections"),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              // Display selected time
-              Row(
-                children: [
-                  const Icon(Icons.more_time, color: Colors.teal),
-                  const SizedBox(width: 10),
-                  Text(
-                    "Departure Time: ${selectedTime.hour}:${selectedTime.minute}",
-                    style: const TextStyle(fontSize: 16, color: Colors.teal),
+        return StatefulBuilder(
+          builder: (context, setState) {
+          return ValueListenableBuilder<bool>(
+            valueListenable: isRouteGeometryInitializedNotifier,
+            builder: (context, isInitialized, _) {
+              return AlertDialog(
+                title: const Text("Confirm your selections"),
+                content: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    // Display selected time
+                    Row(
+                      children: [
+                        const Icon(Icons.more_time, color: Colors.teal),
+                        const SizedBox(width: 10),
+                        Text(
+                          "Departing at: ${selectedTime.hour}:${selectedTime.minute}",
+                          style: const TextStyle(fontSize: 16, color: Colors.teal),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 10),
+              
+                    // Display selected place
+                    Row(
+                      children: [
+                        const Icon(Icons.place, color: Colors.teal),
+                        const SizedBox(width: 10),
+                        Text(
+                          "From: ${selectedPlace?.name}",
+                          style: const TextStyle(fontSize: 16, color: Colors.teal),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                actions: [
+                  TextButton(
+                    child: const Text("Cancel"),
+                    onPressed: () {
+                      Navigator.of(context).pop(); // Close the confirmation dialog
+                    },
                   ),
-                ],
-              ),
-              const SizedBox(height: 10),
+                  TextButton(
+                    onPressed: isInitialized
+                        ? () {
+                            _calculateMeetingPoint();
+                                  isRouteGeometryInitializedNotifier.value = false;
 
-              // Display selected place
-              Row(
-                children: [
-                  const Icon(Icons.place, color: Colors.teal),
-                  const SizedBox(width: 10),
-                  Text(
-                    "Place: ${selectedPlace?.name}",
-                    style: const TextStyle(fontSize: 16, color: Colors.teal),
-                  ),
-                ],
-              ),
-            ],
+                            Navigator.of(context)
+                                .popUntil((route) => route.isFirst);
+                          }
+                        : null,
+                                  style: TextButton.styleFrom(
+            // Optionally change the color while disabled
+            foregroundColor: isInitialized ? Colors.teal : Colors.grey, // Confirm = teal, Calculating = grey
           ),
-          actions: [
-            TextButton(
-              child: const Text("Cancel"),
-              onPressed: () {
-                Navigator.of(context).pop(); // Close the confirmation dialog
-              },
-            ),
-            TextButton(
-              onPressed: isRouteGeometryInitialized ? () {
-                // Call the method to calculate the meeting point
-                _calculateMeetingPoint();
-                setState(() {
-                isRouteGeometryInitialized = false;
-
-                  
-                });
-
-                // Close the dialog and return to the map view
-                Navigator.of(context).popUntil((route) => route.isFirst);
-              } : null,
-                            child: const Text("Confirm"),
-
-            ),
-          ],
-        );
+                    child: Text(isInitialized ? "Confirm" : "Calculating..."),
+                  ),
+                ],
+              );
+            }
+          );
+        });
       },
     );
   }
@@ -410,16 +425,18 @@ class _FerryTrafficScreenState extends State<FerryTrafficScreen> {
     route = routeResult.routes.first;
     routeGeometry = route.routeGeometry;
 
-
     if (routeGeometry != null) {
       final craignureRouteGraphic =
           Graphic(geometry: routeGeometry, symbol: routeLineSymbol);
       _craignureRouteGraphicsOverlay.graphics.add(craignureRouteGraphic);
 
-        setState(() {
-        isRouteGeometryInitialized = true;
+      // Update the ValueNotifier to true, notifying all listeners
+      isRouteGeometryInitializedNotifier.value = true;
 
-    });
+      //     setState(() {
+      //     isRouteGeometryInitialized = true;
+
+      // });
     }
   }
 
@@ -456,11 +473,10 @@ class _FerryTrafficScreenState extends State<FerryTrafficScreen> {
     List<double> listOfMeetingDistances = [];
     var ferryTimesInRange =
         ferrySchedule.getFerryDeparturesInRange(selectedTime);
-        print(ferryTimesInRange.length);
-        for (TimeOfDay ferryTime in ferryTimesInRange){
-          print(ferryTime);
-
-        }
+    print(ferryTimesInRange.length);
+    for (TimeOfDay ferryTime in ferryTimesInRange) {
+      print(ferryTime);
+    }
 
     double toDouble(TimeOfDay myTime) => myTime.hour + myTime.minute / 60.0;
 
